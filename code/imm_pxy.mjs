5ee3fccdcc8d15bd4a23f2c8575cce675b9261b8
Shane Holloway <shane.holloway@ieee.org>
2020-11-05 09:47:48 -0700

    Added imm_pxy.imm_pxy_attr(el)

--- code/imm_pxy.mjs
+++ code/imm_pxy.mjs
@@ -12,3 +12,9 @@ export const imm_pxy = (tag_fn, kw=tag_fn) =>
   ({ __proto__: new Proxy(tag_fn, _imm_pxy), ... kw })
 
 export default imm_pxy
+
+export function imm_pxy_attr(el) {
+  let gattr = el.getAttribute.bind(el)
+  return new Proxy({}, { get: (t,k) => gattr(k) })
+}
+
