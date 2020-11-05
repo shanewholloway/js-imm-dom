71368548e3db3f65081c1c6c5499527f933e574b
Shane Holloway <shane.holloway@ieee.org>
2020-11-05 10:15:22 -0700

    Removed default export of imm_pxy and changed name to imm_pxy_tag

--- code/imm_dom_ns.mjs
+++ code/imm_dom_ns.mjs
@@ -1,8 +1,8 @@
 import {imm_svg_tag, imm_tag} from './imm_dom.mjs'
-import {imm_pxy} from './imm_pxy.mjs'
+import {imm_pxy_tag} from './imm_pxy.mjs'
 
-export const imm_html = imm_pxy(imm_tag)
-export const imm_svg = imm_pxy(imm_svg_tag)
+export const imm_html = imm_pxy_tag(imm_tag)
+export const imm_svg = imm_pxy_tag(imm_svg_tag)
 
 export {
   imm_html as default,
