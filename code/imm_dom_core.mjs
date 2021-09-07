6d41c713ff68f77d1cb3456e0f0709da84aaf4dc
Shane Holloway <shane.holloway@ieee.org>
2021-09-07 16:22:35 -0600

    Added imm_emit support for cancelable, bubbles, and composed event init options

--- code/imm_dom_core.mjs
+++ code/imm_dom_core.mjs
@@ -76,7 +76,7 @@ export function imm_dom(host, namespaceURI) {
   return tag_fn.tag = tag_fn
 }
 
-export function imm_emit(tgt, evt, detail) {
+export function imm_emit(tgt, evt, detail, opt) {
   tgt.dispatchEvent(
-    new CustomEvent(evt, {detail})) }
+    new CustomEvent(evt, {...opt, detail})) }
 
