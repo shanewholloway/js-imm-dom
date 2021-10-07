2702151ea730ab7996f302f809af865413556568
Shane Holloway <shane.holloway@ieee.org>
2021-10-07 09:42:01 -0600

    Added umd export for simple prototyping

--- rollup.config.js
+++ rollup.config.js
@@ -2,39 +2,39 @@ import { terser as rpi_terser } from 'rollup-plugin-terser'
 
 const _rpi_min_ = [ rpi_terser() ]
 
-const configs = []
-export default configs
+export default [
+  ... add_mjs('imm_pxy'),
+  ... add_mjs('imm_raf'),
+
+  ... add_mjs('imm_async'),
+  ... add_mjs('imm_evt_core'),
+  ... add_mjs('imm_evt'),
+
+  ... add_mjs('imm_dom_core'),
+  ... add_mjs('imm_dom'),
+  ... add_mjs('imm_dom_ns'),
+
+  ... add_mjs('imm_tmpl_core'),
+  ... add_mjs('imm_tmpl'),
+
+  ... add_mjs('imm_elem_core'),
+  ... add_mjs('imm_elem'),
+  ... add_mjs('imm_elem_auto'),
+  ... add_mjs('imm_elem_raf'),
+
+  ... add_mjs('index', 'imm_dom'),
+]
 
 
-add_mjs('imm_pxy')
-add_mjs('imm_raf')
-
-add_mjs('imm_async')
-add_mjs('imm_evt_core')
-add_mjs('imm_evt')
-
-add_mjs('imm_dom_core')
-add_mjs('imm_dom')
-add_mjs('imm_dom_ns')
-
-add_mjs('imm_tmpl_core')
-add_mjs('imm_tmpl')
-
-add_mjs('imm_elem_core')
-add_mjs('imm_elem')
-add_mjs('imm_elem_auto')
-add_mjs('imm_elem_raf')
-
-add_mjs('index')
-
-
-function add_mjs(src_name) {
+function * add_mjs(src_name, umd_module) {
   const input = `code/${src_name}.mjs`
-  configs.push(
-    { input, plugins: [],
-      output: { file: `esm/${src_name}.mjs`, format: 'es', sourcemap: true }},
+  yield ({ input, plugins: [], output: [
+      { file: `esm/${src_name}.mjs`, format: 'es', sourcemap: true },
+      umd_module && { file: `umd/${umd_module}.js`, format: 'umd', name: umd_module, sourcemap: true },
+    ].filter(Boolean)})
 
-    { input, plugins: _rpi_min_,
-      output: { file: `esm/${src_name}.min.mjs`, format: 'es', sourcemap: false }},
-  )
+  yield ({ input, plugins: _rpi_min_, output: [
+      { file: `esm/${src_name}.min.mjs`, format: 'es', sourcemap: false },
+      umd_module && { file: `umd/${umd_module}.min.js`, format: 'umd', name: umd_module, sourcemap: false },
+    ].filter(Boolean)})
 }
