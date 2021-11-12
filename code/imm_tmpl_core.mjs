4431b819d0d26ed8d271438c5b27f83d044eb006
Shane Holloway <shane.holloway@ieee.org>
2021-11-12 14:58:33 -0700

    Bugfix for imm_tmpl for constrained parents like select or option

--- code/imm_tmpl_core.mjs
+++ code/imm_tmpl_core.mjs
@@ -1,7 +1,7 @@
 // _tkey prefix from valid custom element name of [4.13.3: Core concepts][WHATWG ]
 //
 // [WHATWG]: https://html.spec.whatwg.org/multipage/custom-elements.html#custom-elements-core-concepts
-const _tkey = 'I\u2133-'+Date.now(), _tqsel = '['+_tkey+']'
+const _tkey = 'I\u2133-'+Date.now(), _tqsel = '['+_tkey+']', _ttype = 'text/x-'+_tkey
 
 export function imm_tmpl_link(invoke_arg) {
   let wm_cache = new WeakMap()
@@ -14,16 +14,16 @@ export function imm_tmpl_link(invoke_arg) {
         el = _imm_tmpl_c(parts))
 
     el = el.content.cloneNode(true)
-    let nodes = el.querySelectorAll(_tqsel)
-    for (let idx=0; idx < nodes.length; idx++) {
-      let tgt_elem = nodes[idx], a = args[idx]
-
+    for (let tgt_elem of el.querySelectorAll(_tqsel)) {
       // remove the tag key -- intentionally ugly!
+      let idx = + tgt_elem.getAttribute(_tkey)
       tgt_elem.removeAttribute(_tkey)
+
+      let a = args[+idx]
 
       // update/replace target node with arg
       invoke_arg(
-        tgt_elem, _tkey === tgt_elem.tagName,
+        tgt_elem, _ttype === tgt_elem.type,
         null == a ? a : a.valueOf(), idx)
     }
     return el
@@ -54,20 +54,21 @@ export function _imm_tmpl_r(parts, kinds) {
   // Concatenates `parts` with attribute or node
   // driven by corresponding `kind`
 
-  let s='', i=0
+  let s='', i=-1
   for (let p of parts) {
     s += p
 
-    let pk = kinds[i] | 0
+    let pk = kinds[++i] | 0
+    if (pk) {
+      // as attribute
+      let pi = ` ${_tkey}=${i}`
 
-    // as attribute
-    let pi = pk ? ` ${_tkey}=${i++}` : ''
+      if (1 & pk) // as node
+        pi = `<SCRIPT type='${_ttype}' ${pi}>\<\/SCRIPT>`
 
-    if (1 & pk) // as node
-      pi = `<${_tkey} ${pi}></${_tkey}>`
-
-    // join with attribute or node
-    s += pi
+      // join with attribute or node
+      s += pi
+    }
   }
 
   // render into template innerHTML
