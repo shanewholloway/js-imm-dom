975cf147aa279e25d1900989141204f54cad2af5
Shane Holloway <shane.holloway@ieee.org>
2021-11-13 15:17:59 -0700

    Changed ImmIter to reset existing generator upon _render_(true)

--- code/imm_elem_iter.mjs
+++ code/imm_elem_iter.mjs
@@ -1,28 +1,30 @@
 import {ImmElem} from './imm_elem_core.mjs'
 
 export class ImmIter extends ImmElem {
-  disconnectedCallback() { 
-    let g = this._g_
-    if (g) {
-      this._g_ = null
-      g.return()
-    }
+  disconnectedCallback() {
+    this._greset_(true)
   }
 
   _render_(is_new) {
     let g = this._g_
     if (!g || is_new)
-      this._g_ = g =
-        this.render(this._ns_, this, this._tgt_)
+      g = this._greset_()
 
     this._gshow_(g.next())
+  }
+
+  _greset_(stop) {
+    let g = this._g_
+    if (g) g.return()
+    return this._g_ = stop ? null
+      : this.render(this._ns_, this, this._tgt_)
   }
 
   async _gshow_(tip) {
     if (tip.then)
       tip = await tip
     if (tip.done)
-      this._g_ = null
+      this._g_ = null // reset next render
     return this._show_(tip.value)
   }
 }
