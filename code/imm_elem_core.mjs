ecffaa82b8a9f814fdf3fdaa84d5c0fc99b09c9e
Shane Holloway <shane.holloway@ieee.org>
2021-11-17 17:25:37 -0700

    Another refactor of ImmElem implementation

--- code/imm_elem_core.mjs
+++ code/imm_elem_core.mjs
@@ -23,92 +23,96 @@ export class ImmCore extends HTMLElement {
   }
 }
 
-export class ImmElem extends ImmCore {
-  init(/* ns, el */) {}
-  render(/* ns, el */) { /* return element */ }
 
+export class ImmElem extends ImmCore {
+  init(/* ns, el, tgt */) { /* return _tgt_ (optional) */ }
+  render(/* ns, el, tgt */) { /* return element to _show_() onto _tgt_ */ }
 
   //--------------------------
   // function-based defintions
 
-  init_dom() {}
-  static dom(tag_name, ... fn_v) {
+  static dom(tag_name, ... args) {
     return this // klass
-      ._imm_c(fn_v, proto => { proto._init_tgt_ = proto.init_dom })
+      ._imm_c(args)
       .define(tag_name)
   }
 
-  init_elem() { return this.attachShadow({mode: 'open'}) }
-  static elem(tag_name, ... fn_v) {
+  static elem(tag_name, ... args) {
+    args.push({_tgt_: 0})
     return this // klass
-      ._imm_c(fn_v, proto => { proto._init_tgt_ = proto.init_elem })
+      ._imm_c(args)
       .define(tag_name)
   }
 
-  static _imm_c(fn_v, with_proto) {
-    let ImmCE = class extends this {}
-    let proto = ImmCE.prototype
-    with_proto(proto, ImmCE)
-    return ImmCE._imm_cfn(proto, fn_v)
+  static _imm_c(args) {
+    let klass = class extends this {}
+    args.reduceRight(klass._imm_cz,
+      {r:klass.prototype,
+       z:['init', 'render']})
+    return klass
   }
-  static _imm_cfn(proto, fn_v) {
-    // render is last argument
-    proto.render = fn_v.pop() || proto.render
-    // init is optional first argument
-    proto.init = fn_v.shift() || proto.init
-    return this
+  static _imm_cz(ctx, v) {
+    // _imm_c optional args as reduceRight algorithm
+    if ('function' === typeof v)
+      ctx.r[ctx.z.pop()] = v
+    else Object.assign(ctx.r, v)
+    return ctx
   }
 
 
   //--------------------------------------
   // web component composed implementation
 
-  constructor() { super(); this._init_() }
-  connectedCallback() { this._render_(true) }
+  connectedCallback() {
+    if (this._bind_)
+      this._init_()
+
+    this._render_(true)
+  }
   attributeChangedCallback() { this._refresh_() }
 
-  //-----------------
-  // composed methods
+  _init_() {
+    Object.assign(this, this._bind_(), {_bind_:0})
+    let tgt = this._tgt_ = this._init_tgt_(this) || this
+    this._tgt_ = this.init(this._ns_, this, tgt) || tgt
+  }
+  _init_tgt_({_tgt_}) {
+    return 0 === _tgt_ ? this.attachShadow({mode: 'open'}) : this
+  }
 
   get _ns_() { return imm_pxy_attr(this) }
-  async _init_() {
-    this._tgt_ = this._init_tgt_(this) || this
-    await 0 // next micro tick
-    this._bind_()
-
-    let tgt = this.init(this._ns_, this, this._tgt_)
-    if (null != tgt)
-      this._tgt_ = tgt.then ? await tgt : tgt
-  }
-  _bind_() {
-    this._show_ = this._show_.bind(this)
-    this._refresh_ = p => p && p.then
-      ? p.then(this._refresh_)
-      : this._render_()
-  }
-  _init_tgt_() {}
 
   _render_(/* is_new */) {
     this._show_(
       this.render(this._ns_, this, this._tgt_) )
   }
 
-  _show_(node) {
-    let tgt = this._tgt_
+  _bind_() {
+    return ({
+      _refresh_: p => p && p.then
+        ? p.then(this._refresh_)
+        : this._render_(),
 
-    if (this === node || tgt === node || null == node) {
-      if (null === node)
+      _show_: node => {
+        let tgt = this._tgt_
+
+        if (this === node || tgt === node || null == node) {
+          if (null === node)
+            tgt.textContent = '' // clear all inner content (text and html)
+          return // no-op
+        }
+
+        if (node.then) // async promise render
+          return node.then(this._show_)
+
+        // inlined optimized version of imm_set()
         tgt.textContent = '' // clear all inner content (text and html)
-      return // no-op
-    }
-
-    if (node.then) // async promise render
-      return node.then(this._show_)
-
-    // inlined optimized version of imm_set()
-    tgt.textContent = '' // clear all inner content (text and html)
-    tgt.append(node)
+        tgt.append(node)
+      },
+    })
   }
 }
 
 export default ImmElem
+
+
--- docs/compressed.md
+++ docs/compressed.md
@@ -2,10 +2,10 @@
 
 | module          |   brotli | minified |   source |
 |:----------------|---------:|---------:|---------:|
-| `index`         |   2206 B |   5462 B |  11485 B |
-| `imm_elem`      |    549 B |   1375 B |   2710 B |
-| `imm_tmpl`      |    830 B |   1745 B |   4635 B |
-| `imm_dom`       |    520 B |   1056 B |   2220 B |
+| `index`         |   2231 B |   5449 B |  11516 B |
+| `imm_elem`      |    570 B |   1362 B |   2741 B |
+| `imm_tmpl`      |    829 B |   1745 B |   4635 B |
+| `imm_dom`       |    523 B |   1056 B |   2220 B |
 
 
 ## Size of Immediate-mode DOM tools
@@ -13,18 +13,18 @@
 | module          |   brotli | minified |   source |
 |:----------------|---------:|---------:|---------:|
 | `imm_async`     |    209 B |    346 B |    630 B |
-| `imm_dom`       |    520 B |   1056 B |   2220 B |
-| `imm_dom_core`  |    479 B |    968 B |   2066 B |
+| `imm_dom`       |    523 B |   1056 B |   2220 B |
+| `imm_dom_core`  |    484 B |    968 B |   2066 B |
 | `imm_dom_ns`    |    621 B |   1220 B |   2598 B |
-| `imm_elem`      |    549 B |   1375 B |   2710 B |
-| `imm_elem_core` |    587 B |   1468 B |   2859 B |
-| `imm_elem_iter` |    641 B |   1678 B |   3220 B |
-| `imm_elem_raf`  |    829 B |   2101 B |   4264 B |
+| `imm_elem`      |    570 B |   1362 B |   2741 B |
+| `imm_elem_core` |    606 B |   1455 B |   2890 B |
+| `imm_elem_iter` |    659 B |   1665 B |   3251 B |
+| `imm_elem_raf`  |    852 B |   2088 B |   4295 B |
 | `imm_evt`       |    317 B |    651 B |   1099 B |
 | `imm_evt_core`  |    156 B |    314 B |    524 B |
 | `imm_pxy`       |    244 B |    392 B |    761 B |
 | `imm_raf`       |    194 B |    284 B |    556 B |
-| `imm_tmpl`      |    830 B |   1745 B |   4635 B |
+| `imm_tmpl`      |    829 B |   1745 B |   4635 B |
 | `imm_tmpl_core` |    410 B |    722 B |   2180 B |
-| `index`         |   2206 B |   5462 B |  11485 B |
+| `index`         |   2231 B |   5449 B |  11516 B |
 
