f75ad20351dc83a75cabf0a739c83461ebad1cde
Shane Holloway <shane.holloway@ieee.org>
2022-04-28 11:39:07 -0600

    Added imm_wcqx to do imm_qs across shadowDOM boundaries

--- code/imm_query.mjs
+++ code/imm_query.mjs
@@ -2,6 +2,11 @@ export const imm_qx = (key, el_ctx) =>
   (el_ctx = el_ctx.closest(`[data-${key}]`)) &&
     el_ctx.querySelector(el_ctx.dataset[key])
 
+export const imm_wchost = el => el.getRootNode().host
+
+export const imm_wcqx = (key, el_ctx) => !el_ctx ? null :
+  imm_qx(key, el_ctx) || imm_wcqx(key, imm_wchost(el_ctx))
+
 export function imm_set_qx(key, el, el_ctx=el.parentNode) {
   let id = el.id || (el.id = `z${2e9*Math.random()|0}`)
   el_ctx.dataset[key] = `${el.tagName}#${id}`
