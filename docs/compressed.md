4beb4ed451c5bd20699313a84b0217a7cc05ba12
Shane Holloway <shane.holloway@ieee.org>
2022-04-27 21:55:10 -0600

    Changed from .mjs to .js exports

--- docs/compressed.md
+++ docs/compressed.md
@@ -2,8 +2,8 @@
 
 | module          |   brotli | minified |   source |
 |:----------------|---------:|---------:|---------:|
-| `index`         |   2796 B |   7031 B |  14471 B |
-| `imm_elem`      |    878 B |   2090 B |   4105 B |
+| `index`         |   2849 B |   7285 B |  14931 B |
+| `imm_elem`      |    940 B |   2254 B |   4473 B |
 | `imm_tmpl`      |    834 B |   1790 B |   4757 B |
 | `imm_dom`       |    559 B |   1114 B |   2382 B |
 
@@ -13,19 +13,19 @@
 | module          |   brotli | minified |   source |
 |:----------------|---------:|---------:|---------:|
 | `imm_async`     |    145 B |    212 B |    405 B |
-| `imm_clone`     |    218 B |    418 B |    636 B |
+| `imm_clone`     |    241 B |    481 B |    737 B |
 | `imm_css`       |    879 B |   1876 B |   3881 B |
 | `imm_dom`       |    559 B |   1114 B |   2382 B |
 | `imm_dom_core`  |    402 B |    780 B |   1770 B |
 | `imm_dom_ns`    |    638 B |   1265 B |   2785 B |
-| `imm_elem`      |    878 B |   2090 B |   4105 B |
-| `imm_elem_core` |    915 B |   2183 B |   4254 B |
-| `imm_elem_raf`  |   1043 B |   2501 B |   5127 B |
+| `imm_elem`      |    940 B |   2254 B |   4473 B |
+| `imm_elem_core` |    975 B |   2362 B |   4613 B |
+| `imm_elem_raf`  |   1104 B |   2665 B |   5495 B |
 | `imm_evt`       |    248 B |    517 B |    874 B |
 | `imm_evt_core`  |    156 B |    314 B |    524 B |
 | `imm_pxy`       |    319 B |    621 B |   1177 B |
 | `imm_raf`       |    208 B |    314 B |    602 B |
 | `imm_tmpl`      |    834 B |   1790 B |   4757 B |
 | `imm_tmpl_core` |    410 B |    722 B |   2180 B |
-| `index`         |   2796 B |   7031 B |  14471 B |
+| `index`         |   2849 B |   7285 B |  14931 B |
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,47 @@
 import { terser as rpi_terser } from 'rollup-plugin-terser'
 
-const _rpi_min_ = [ rpi_terser() ]
+let is_watch = process.argv.includes('--watch')
+const _rpi_min_ = is_watch ? [] : [ rpi_terser() ]
 
 export default [
-  ... add_mjs('imm_pxy'),
-  ... add_mjs('imm_raf'),
+  ... add_esm('imm_pxy'),
+  ... add_esm('imm_raf'),
 
-  ... add_mjs('imm_async'),
-  ... add_mjs('imm_evt_core'),
-  ... add_mjs('imm_evt'),
+  ... add_esm('imm_async'),
+  ... add_esm('imm_evt_core'),
+  ... add_esm('imm_evt'),
 
-  ... add_mjs('imm_dom_core'),
-  ... add_mjs('imm_dom'),
-  ... add_mjs('imm_dom_ns'),
-  ... add_mjs('imm_css'),
+  ... add_esm('imm_dom_core'),
+  ... add_esm('imm_dom'),
+  ... add_esm('imm_dom_ns'),
+  ... add_esm('imm_css'),
 
-  ... add_mjs('imm_tmpl_core'),
-  ... add_mjs('imm_tmpl'),
+  ... add_esm('imm_tmpl_core'),
+  ... add_esm('imm_tmpl'),
 
-  ... add_mjs('imm_elem_core'),
-  ... add_mjs('imm_elem'),
-  ... add_mjs('imm_elem_raf'),
-  ... add_mjs('imm_clone'),
+  ... add_esm('imm_elem_core'),
+  ... add_esm('imm_elem'),
+  ... add_esm('imm_elem_raf'),
+  ... add_esm('imm_clone'),
 
-  ... add_mjs('beta/imm_elem_auto'),
-  ... add_mjs('beta/imm_elem_iter'),
+  ... add_esm('beta/imm_elem_auto'),
+  ... add_esm('beta/imm_elem_iter'),
 
-  ... add_mjs('index', 'imm_dom'),
+  ... add_esm('index', 'imm_dom'),
 ]
 
 
-function * add_mjs(src_name, umd_module) {
+function * add_esm(src_name, umd_module) {
   const input = `code/${src_name}.mjs`
   yield ({ input, plugins: [], output: [
       { file: `esm/${src_name}.mjs`, format: 'es', sourcemap: true },
+      { file: `esm/${src_name}.js`, format: 'es', sourcemap: true },
       umd_module && { file: `umd/${umd_module}.js`, format: 'umd', name: umd_module, sourcemap: true },
     ].filter(Boolean)})
 
   yield ({ input, plugins: _rpi_min_, output: [
       { file: `esm/${src_name}.min.mjs`, format: 'es', sourcemap: false },
+      { file: `esm/${src_name}.min.js`, format: 'es', sourcemap: false },
       umd_module && { file: `umd/${umd_module}.min.js`, format: 'umd', name: umd_module, sourcemap: false },
     ].filter(Boolean)})
 }
