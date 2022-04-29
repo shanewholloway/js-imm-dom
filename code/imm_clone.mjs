a117d7fc24af74e4c32ccb243a5d774bc87e9e10
Shane Holloway <shane.holloway@ieee.org>
2022-04-29 11:02:25 -0600

    Refactored imm_clone to use Imm1

--- code/imm_clone.mjs
+++ code/imm_clone.mjs
@@ -1,25 +1,12 @@
+import { Imm1 } from './imm_elem_core.mjs'
 
-export class ImmClone extends HTMLElement {
-  connectedCallback() { this.render() }
-  render() {
-    this.textContent = '' // inline clear children
-    imm_clone(this, this.getAttribute('query'))
-  }
-  static define(tag_name, opt) {
-    customElements.define(tag_name, this, opt)
-    return this
-  }
-}
-
-export class ImmCloneEx extends ImmClone {
-  static get observedAttributes() { return ['query'] }
-  attributeChangedCallback() { this.render() }
+export class ImmClone extends Imm1 {
+  update() { imm_clone(this, this.getAttribute('query')) }
 }
 
 export function imm_clone(el_tgt, query, el_root=el_tgt.ownerDocument) {
-  for (let el of el_root.querySelectorAll(query || '[data-shadow]')) {
+  for (let el of el_root.querySelectorAll(query)) {
     el = (el.content || el).cloneNode(true)
     el_tgt.append(el)
   }
 }
-
