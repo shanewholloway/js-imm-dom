515b38251a3243964025c3d7bfb91ffc2ba84b88
Shane Holloway <shane.holloway@ieee.org>
2022-04-29 11:02:14 -0600

    Refactored imm_elem_qx to use Imm1/ImmNS

--- code/imm_elem_qx.mjs
+++ code/imm_elem_qx.mjs
@@ -1,11 +1,9 @@
-import { Imm0 } from './imm_elem_core.mjs'
-import { imm_pxy_attr } from './imm_pxy.mjs'
+import { ImmNS } from './imm_elem_core.mjs'
 import { imm_set_qx } from './imm_query.mjs'
 export { imm_set_qx, imm_qx, imm_wcqx } from './imm_query.mjs'
 
-export class ImmQX extends Imm0 {
-  get _ns_() { return imm_pxy_attr(this) }
-  connectedCallback() { this.set_qx() }
+export class ImmQX extends ImmNS {
+  update() { this.set_qx() }
   set_qx(key=this.qx||this.tagName) {
     imm_set_qx(key, this, this.qx_parent)
   }
