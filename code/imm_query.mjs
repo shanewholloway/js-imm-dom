2f1e7d1400b1c215b36b60ab2dea082efd13517d
Robert Sirois <rpsirois@gmail.com>
2022-07-06 10:55:34 -0600

    Fixed imm_wcqx to throw an error if el_ctx is not provided

--- code/imm_query.mjs
+++ code/imm_query.mjs
@@ -1,4 +1,4 @@
-export const imm_wchost = el => el.getRootNode().host
+export const imm_wchost = el => el.getRootNode().host || null
 
 export const imm_id = (el, id=`z${2e9*Math.random()|0}`) =>
   el.id ||= id
@@ -10,6 +10,6 @@ export const imm_qx = (key, el_ctx) =>
   (el_ctx = el_ctx.closest(`[data-${key}]`)) &&
     el_ctx.querySelector(el_ctx.dataset[key])
 
-export const imm_wcqx = (key, el_ctx) => !el_ctx ? null :
+export const imm_wcqx = (key, el_ctx) => null !== el_ctx &&
   imm_qx(key, el_ctx) || imm_wcqx(key, imm_wchost(el_ctx))
 
