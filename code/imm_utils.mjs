76c83a2238b12a87e9462747b392c7ed971cad02
Shane Holloway <shane.holloway@ieee.org>
2022-07-13 10:12:25 -0600

    Made _dash_name and _prop_name resilliant to nullish

--- code/imm_utils.mjs
+++ code/imm_utils.mjs
@@ -1,6 +1,6 @@
 export const
-  _dash_name = s => s.replace(/_/g, '-'),
-  _prop_name = s => s.replace(/-/g, '_'),
+  _dash_name = s => s?.replace(/_/g, '-'),
+  _prop_name = s => s?.replace(/-/g, '_'),
   _is_obj = a => 'object' === typeof a && null !== a,
   _is_iter = a => _is_obj(a) && Symbol.iterator in a,
   _is_attr_dict = a => _is_obj(a)
@@ -20,3 +20,4 @@ export const
 export const
   _elr_evt = (el,args) => (el.addEventListener(...args), el),
   _elr_devt = (el,[evt,fn]) => _elr_evt(el, [evt, e=>fn(e.detail)])
+
