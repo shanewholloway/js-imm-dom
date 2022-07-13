eab0d075aa75086b4a87ed2c9d1ba45b708236c9
Shane Holloway <shane.holloway@ieee.org>
2022-07-13 08:51:02 -0600

    Remove _imm_c from imma logic

--- code/imm_dom_async.mjs
+++ code/imm_dom_async.mjs
@@ -1,4 +1,4 @@
-import {imm, _imm_c} from './imm_dom_core.mjs'
+import {imm} from './imm_dom_core.mjs'
 export {imm, imm_set} from './imm_dom_core.mjs'
 
 export const imma = (el_placeholder, el_promise, attrs) =>
@@ -6,6 +6,6 @@ export const imma = (el_placeholder, el_promise, attrs) =>
   , el_placeholder)
 
 async function _imma(el_placeholder, el_promise, attrs) {
-  el_promise = imm(_imm_c(await el_promise), attrs)
+  el_promise = imm(await el_promise, await attrs)
   el_placeholder?.replaceWith(el_promise)
 }
