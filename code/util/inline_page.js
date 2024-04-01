ad753214c3624c4f2d50cb4140cca97f2811ac9f
Shane Holloway <shane.holloway@ieee.org>
2024-04-01 00:21:34 -0600

    Exported doc_inline_remove method util/inline_page.js

--- code/util/inline_page.js
+++ code/util/inline_page.js
@@ -3,17 +3,18 @@ import { dom_as_html_src, mime_html, as_download } from './blob.js'
 import { doc_inline_css } from './inline_css.js'
 import { doc_inline_js } from './inline_js.js'
 
-
-export async function doc_inline_page(el_root=window.document) {
+export function doc_inline_remove(el_root=window.document) {
   for (let el of el_root.querySelectorAll('[data-inline-remove]'))
     el.remove()
-
-  await _inline_all_done(el_root,
-    doc_inline_css(el_root),
-    doc_inline_js(el_root))
-
-  return dom_as_html_src(el_root)
+  return el_root
 }
+
+export const doc_inline_page = (el_root=window.document) =>
+  _inline_all_done(el_root,
+      doc_inline_remove(el_root),
+      doc_inline_css(el_root),
+      doc_inline_js(el_root),
+    ).then(dom_as_html_src)
 
 export const doc_inline_page_download = (filename, el_root=window.document) =>
   as_download(
